908550ff07eaa80c01a5d0c5ff1739d46022439d
dhrutube <dhruvsehgal2554@gmail.com>
2026-05-20 01:26:21 -0700

    updated / 60 from // 60

--- src/features.py
+++ src/features.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # loading clean data
 df = pd.read_csv('data/transformed/events.csv')
 # adding column duration_minutes
-df['duration_minutes'] = df['duration_seconds'] // 60
+df['duration_minutes'] = df['duration_seconds'] / 60
 # adding weekday column
 df['weekday'] = pd.to_datetime(df['date']).dt.day_name()
 # writing transformed data
